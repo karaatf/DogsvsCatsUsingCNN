a107f9df91a8c6695d4350e9d895a147c2698e47
karaatf <atfkara19@gmail.com>
2024-06-07 17:19:01 +0300

    CNN test

--- DOGS_VS_CATS_USİNG_CNN.py
+++ DOGS_VS_CATS_USİNG_CNN.py
@@ -1,82 +1,38 @@
-import os,shutil
-from matplotlib import pyplot as plt
 from tensorflow import keras
-from keras import layers,models
-from keras import optimizers
+import matplotlib.pyplot as plt
+import pandas as pd
 
+Data = keras.datasets.fashion_mnist.load_data()
+(x_train,y_train),(x_test,y_test)=Data
+print(x_train[23])
 
-print("total cat images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/train_cats")))
-print("total dogs images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/train_dogs")))
-print("total val_cats images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/val_cats")))
-print("total val_dogs images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/val_dogs")))
-print("total test_cats images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/test_cats")))
-print("total test_dogs images",len(os.listdir("/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/test_dogs")))
+plt.imshow(x_train[1],cmap="gray")
 
+x_train = x_train/255.0
+x_test = x_test/255.0
+print(y_train[1])
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+print(y_train[1])
 
+model = keras.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28,28]))
+model.add(keras.layers.Dense(300,activation="relu"))
+model.add(keras.layers.Dense(300,activation="relu"))
+model.add(keras.layers.Dense(10,activation="softmax"))
 
-model = models.Sequential()
-model.add(layers.Conv2D(32,(3,3),activation="relu",input_shape=(150,150,3)))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(64,(3,3),activation="relu"))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation="relu"))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Conv2D(128,(3,3),activation="relu"))
-model.add(layers.MaxPool2D((2,2)))
-model.add(layers.Flatten())
-model.add(layers.Dense(512,activation="relu"))
-model.add(layers.Dense(1,activation="sigmoid"))
-
-
-model.summary()
-
-
-
-model.compile(optimizer="rmsprop",
-              loss="binary_crossentropy",
+model.compile(loss="categorical_crossentropy",
+              optimizer=keras.optimizers.SGD(),
               metrics=["accuracy"])
 
-from keras.preprocessing.image import ImageDataGenerator as IDG
+history = model.fit(x_train,y_train,epochs=30,validation_split=0.1)
 
-
-train_data_gen= IDG(rescale=1./255)
-test_data_gen= IDG(rescale=1./255)
-
-train_generator=train_data_gen.flow_from_directory(
-    "/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/train",
-    target_size=(150,150),
-    batch_size=20,
-    class_mode="binary")
-
-val_generator=test_data_gen.flow_from_directory(
-    "/content/drive/MyDrive/Colab Notebooks/dogs_vs_cats_first_1000_examples/val",
-    target_size=(150,150),
-    batch_size=20,
-    class_mode="binary")
-
-
-for data_batch,label_batch in train_generator:
-  print("data_bach_size:",data_batch.shape)
-  print("labels batch size:",label_batch.shape)
-  break
-
-history = model.fit(train_generator, steps_per_epoch=100, epochs=30, validation_data=val_generator,validation_steps=50)
-
-
-model.save("cat_and_dog_small_1.h5")
-
-history=history.history
-history_dic = history
-loss_values = history_dic["loss"]
-val_loss_val=history_dic["val_loss"]
-epochs = range(1,len(loss_values)+1)
-
-
-plt.clf()#şekli temizler
-plt.plot(epochs,loss_values,"bo",label="Eğitim kaybı")
-plt.plot(epochs,val_loss_val,"b",label="Doğruluk kaybı")
-plt.title("Eğitim veDoğruluk kaybı")
-plt.xlabel("epoklar")
-plt.ylabel("kayıp")
-plt.legend()
+pd.DataFrame(history.history).plot(figsize=(8,5))
+plt.grid(True)
+plt.gca().set_ylim(0,1)
 plt.show()
+
+model.evaluate(x_test,y_test)
+
+a=model.predict(x_test[:2])
+print(a)
